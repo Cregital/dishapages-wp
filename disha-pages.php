2f5ab2c112c64c1762c16494c67ce170b209130a
Rufus Oyemade <me@oluwarufus.com>
2020-03-09 12:50:54 +0100

    Updated license

--- disha-pages.php
+++ disha-pages.php
@@ -6,6 +6,21 @@ Description: Simple plugin to show your Disha page on Wordpress
 Version: 1.0
 Author: Disha
 Author URI: https://disha.ng
+
+
+Disha Pages WP is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+Disha Pages WP is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with Disha Pages WP. If not, see http://www.gnu.org/licenses/gpl.html.
+
 */
 
 defined( 'ABSPATH' ) or exit;
